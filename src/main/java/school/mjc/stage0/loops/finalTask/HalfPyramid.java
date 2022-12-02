217a0e3ece542522e50e467567545878379e49ce
kokibas <nurym0807@gmail.com>
2022-12-02 11:13:56 +0600

    01

--- src/main/java/school/mjc/stage0/loops/finalTask/HalfPyramid.java
+++ src/main/java/school/mjc/stage0/loops/finalTask/HalfPyramid.java
@@ -2,7 +2,15 @@ package school.mjc.stage0.loops.finalTask;
 
 public class HalfPyramid {
     public void printHalfPyramid(int cathetusLength) {
-
+        for (int i = cathetusLength - 1; i >= 0; i--){
+            for(int k = 1; k < i; k++){
+                System.out.print(" ");
+            }
+            for (int j = i; j <= cathetusLength - 1;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
     }
 
     public static void main(String[] args) {
